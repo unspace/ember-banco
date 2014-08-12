72c6bc51a3862e1ddf04b84ea1c576c8e8b3ac2d
Mattia Gheda <ghedamat@gmail.com>
2014-08-12 09:58:44 -0400

    pretender is in test helpers

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -1,28 +1,5 @@
 import Ember from 'ember';
 import startApp from '../helpers/start-app';
-
-var SESSION = {
-  id:                 'current',
-  name:               'Carsten Nielsen',
-  email:              'carsten@unspace.ca',
-  address:            '1332 Bloor St. West',
-  addressCont:        'Ground Floor',
-  postalCode:         'M6H1P2',
-  city:               'Toronto',
-  province:           'on',
-  dayPhone:           '(416) 977-0269',
-  eveningPhone:       null,
-  wantsNewsletter:    true,
-  wantsPromotions:    false,
-  wantsPartnerEmails: false
-};
-
-var server = new Pretender(function(){
-  this.get('/api/session', function(request){
-    return [200, {"Content-Type": "application/json"}, JSON.stringify({session: SESSION})]
-  });
-});
-
 var App;
 
 module('Acceptance: Index', {
