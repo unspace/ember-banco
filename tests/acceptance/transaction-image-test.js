6aa1f9a1f5b1866c20f3d12bdc838a226f12874e
Mattia Gheda <ghedamat@gmail.com>
2014-08-12 18:31:15 -0400

    missing semicolon

--- tests/acceptance/transaction-image-test.js
+++ tests/acceptance/transaction-image-test.js
@@ -24,7 +24,7 @@ test('it renders the transaction image', function() {
   visit('/accounts/3f946b41-e8c3-4e1b-ac6d-8f3099728fe7/transactions/9f309941-ce83-41eb-c6ad-46b7283f9fe7/image');
 
   andThen(function() {
-    var image = find('.ui-overlay-content img')
+    var image = find('.ui-overlay-content img');
     equal(image.attr('src'), '/assets/cheques/9f309941-ce83-41eb-c6ad-46b7283f9fe7.jpg');
   });
 });
