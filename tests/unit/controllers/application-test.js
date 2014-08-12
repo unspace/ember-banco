ac5cdda097a71888f5f3d9dc612f89f92f939576
Mattia Gheda <ghedamat@gmail.com>
2014-08-12 00:25:13 -0400

    fix application controller test

--- tests/unit/controllers/application-test.js
+++ tests/unit/controllers/application-test.js
@@ -2,7 +2,8 @@ import { test, moduleFor } from 'ember-qunit';
 
 moduleFor('controller:application', 'ApplicationController', {
   setup: function () {},
-  teardown: function () {}
+  teardown: function () {},
+  needs: ['controller:session']
 });
 
 test('it provides the current year', function(){
